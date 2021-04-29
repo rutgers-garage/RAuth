d30cb7627dea6dc2e21b265b84f39286e47f1277
RahulNTrivedi <rahult0705@gmail.com>
2021-04-28 23:57:57 -0400

    frontend work on buttons and routing

--- frontend/rauth/src/App.js
+++ frontend/rauth/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import GoogleLogin from 'react-google-login';
+import Login from "./components/Login.js";
+import Register from "./components/Register.js";
+import Dashboard from "./components/Dashboard.js";
+
+
+class App extends Component {
+  render(){
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Login}/>
+          <Route exact path="/register" component={Register}/>
+          <Route exact path="/dashboard" component={Dashboard}/>
+        </Switch>
+      </BrowserRouter>
+
+    );
+  }
 }
 
 export default App;
---
+++ frontend/rauth/src/components/Login.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import GoogleLogin from 'react-google-login';
+import { Redirect } from "react-router-dom";
+import Register from "./Register.js";
+
+
+
+const responseGoogle = (response) => {
+  console.log(response);
+}
+const success = (response) => {
+  console.log(response["ft"]["Qt"]);
+  sessionStorage.setItem("email", response["ft"]["Qt"]);
+  <Redirect to="/register" />
+}
+
+class Login extends Component {
+  render(){
+    return(
+      <div>
+        <GoogleLogin
+          clientId="928852366939-tps39s15ntonmmrc0pllpj0klionbs7c.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={success}
+          onFailure={responseGoogle}
+          cookiePolicy={'single_host_origin'}
+          hostedDomain="scarletmail.rutgers.edu"
+        />
+      </div>
+    );
+  }
+}
+
+export default Login;
